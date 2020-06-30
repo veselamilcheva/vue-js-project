09c6b30b9ffd511c4e5abb01fb9b2df4840f631a
Vesela Milcheva <milchevavesela@gmail.com>
2020-06-29 21:31:47 -0400

    vue forms

--- vue-components/src/App.vue
+++ vue-components/src/App.vue
@@ -1,69 +1,220 @@
 <template>
-  <div class="container">
-    <div class="row">
-      <div class="col-xs-12">
-        <button @click="selectedComponent = 'appQuote'">Quote</button>
-        <button @click="selectedComponent = 'appAuthor'">Author</button>
-        <button @click="selectedComponent = 'appNew'">New</button>
-        <p>{{ selectedComponent }}</p>
-        <hr />
-        <keep-alive>
-          <component :is="selectedComponent">
-            <h1 slot="title">{{ quoteTitle }}</h1>
-            <p slot="content">Wonderfull quote</p>
-          </component>
-        </keep-alive>
-      </div>
+    <div class="container">
+        <form autocomplete="off">
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                    <h1>File a Complaint</h1>
+                    <hr>
+                    <div class="form-group">
+                        <label for="email">Mail</label>
+                        <input
+                                type="text"
+                                id="email"
+                                class="form-control"
+                                v-model="userData.email">
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input
+                                type="password"
+                                id="password"
+                                class="form-control"
+                                v-model.lazy="userData.password">
+                    </div>
+                    <div class="form-group">
+                        <label for="age">Age</label>
+                        <input
+                                type="number"
+                                id="age"
+                                class="form-control"
+                                v-model="userData.age">
+                    </div>
+
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group">
+                    <label for="message">Message</label><br>
+                    <!-- Interpolation between <textarea>{{ test }}</textarea> doesn't work!-->
+                    <textarea
+                            id="message"
+                            rows="5"
+                            class="form-control"
+                            v-model="message"></textarea>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                    <div class="form-group">
+                        <label for="sendmail">
+                            <input
+                                    type="checkbox"
+                                    id="sendmail"
+                                    value="SendMail"
+                                    v-model="sendMail"> Send Mail
+                        </label>
+                        <label for="sendInfomail">
+                            <input
+                                    type="checkbox"
+                                    id="sendInfomail"
+                                    value="SendInfoMail"
+                                    v-model="sendMail"> Send Infomail
+                        </label>
+                    </div>
+
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group">
+                    <label for="male">
+                        <input
+                                type="radio"
+                                id="male"
+                                value="Male"
+                                v-model="gender"> Male
+                    </label>
+                    <label for="female">
+                        <input
+                                type="radio"
+                                id="female"
+                                value="Female"
+                                v-model="gender"> Female
+                    </label>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 from-group">
+                    <label for="priority">Priority</label>
+                    <select
+                            id="priority"
+                            class="form-control"
+                            v-model="selectedProperty">
+                        <option v-for="property in properties" :key="property" :selected="property === 'Medium'">{{ property }}</option>
+                    </select>
+                </div>
+            </div>
+            <hr>
+            <div class="row">
+              <app-switch v-model="dataSwitch"></app-switch>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                    <button
+                            class="btn btn-primary" @click.prevent="submitted">Submit!
+                    </button>
+                </div>
+            </div>
+        </form>
+        <hr>
+        <div class="row" v-if="isSubmitted">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h4>Your Data</h4>
+                    </div>
+                    <div class="panel-body">
+                        <p>Mail: {{ userData.email }}</p>
+                        <p>Password: test</p>
+                        <p>Age: {{ userData.age }}</p>
+                        <p style="white-space: pre">Message: {{ userData.message }}</p>
+                        <p><strong>Send Mail?</strong></p>
+                        <ul>
+                            <li v-for="item in sendMail" :key="item">{{ item }}</li>
+                        </ul>
+                        <p>Gender: {{ gender }}</p>
+                        <p>Priority:</p>
+                        <p>Switched: {{ dataSwitch }}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <hr>
+        <hr>
+         <form v-if="!submitted">
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                    <app-full-name v-model="fullName"></app-full-name>
+                    <div class="form-group">
+                        <label>Mail</label>
+                        <input type="text" class="form-control" v-model="email">
+                    </div>
+                    <div class="form-group">
+                        <label>Password</label>
+                        <input type="password" class="form-control" v-model="password">
+                    </div>
+                    <div class="form-group">
+                        <label>
+                            <input type="radio" value="Yes" v-model="storeData"> Yes
+                        </label>
+                        <label>
+                            <input type="radio" value="No" v-model="storeData"> No
+                        </label>
+                    </div>
+
+                    <button
+                            type="submit"
+                            @click.prevent="submitted = true"
+                            class="btn btn-primary">Submit the Form</button>
+                </div>
+            </div>
+        </form>
+        <hr>
+        <div class="row" v-if="submitted">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h4>Your Data</h4>
+                    </div>
+                    <div class="panel-body">
+                        <p>Full Name: {{ fullName }}</p>
+                        <p>Mail: {{ email }}</p>
+                        <p>Password: {{ password }}</p>
+                        <p>Store in Database?: {{ storeData }}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
-import Quote from "./components/Quote.vue";
-import Author from "./components/Author.vue";
-import New from "./components/New.vue";
+    import Switch from './Switch.vue';
+    import FullName from './FullName.vue';
 
-export default {
-  data: function() {
-    return {
-      quoteTitle: "Test",
-      selectedComponent: "appQuote"
-    };
-  },
-  components: {
-    appQuote: Quote,
-    appAuthor: Author,
-    appNew: New
-  }
-};
+    export default {
+      data() {
+        return {
+          userData: {
+            email: '',
+            password: '',
+            age: 27
+          },
+          message: '',
+          sendMail: [],
+          gender: 'Female',
+          selectedProperty: 'High',
+          properties: ['High', 'Medium', 'Low'],
+          dataSwitch: true,
+          isSubmitted: false,
+          fullName: 'Vess Milcheva',
+          email: '',
+          password: '',
+          storeData: 'Yes',
+          submitted: false
+        }
+      },
+      methods: {
+        submitted() {
+          this.isSubmitted = true;
+        }
+      },
+      components: {
+        appSwitch: Switch,
+        appFullName: FullName
+      }
+    }
 </script>
 
 <style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 
-h1,
-h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
 </style>
