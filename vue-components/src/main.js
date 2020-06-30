7f27449f731e37600dc79c3a461830be24123516
Vesela Milcheva <milchevavesela@gmail.com>
2020-06-29 23:19:30 -0400

    directives global and local

--- vue-components/src/main.js
+++ vue-components/src/main.js
@@ -1,5 +1,21 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+
+Vue.directive('highlight', {
+  bind(el, binding, vnode) {
+    let delay = 0;
+    if(binding.modifiers['delayed']) {
+      delay = 2000;
+    }
+    setTimeout(() => {
+      if(binding.arg == 'background') {
+        el.style.backgroundColor = binding.value;
+      } else {
+        el.style.color = binding.value;
+      }
+    },delay)
+  }
+})
 
 const vm = new Vue({
   ...App
