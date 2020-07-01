ccee5ab1c228067bd87587027d0f0dc109f960ba
Vesela Milcheva <milchevavesela@gmail.com>
2020-07-01 15:16:01 -0400

    emiting events transfer data from child to child

--- vue-components/src/components/Header.vue
+++
@@ -1,6 +0,0 @@
-<template>
-    <ul class="nav nav-pills">
-    <router-link class="nav-link" to="/" tag="li" active-class="active" exact><a>Home</a></router-link>
-    <router-link class="nav-link" to="/user" tag="li" active-class="active"><a>User</a></router-link>
-</ul>
-</template>
--- vue-components/src/main.js
+++ vue-components/src/main.js
@@ -1,31 +1,8 @@
 import Vue from 'vue';
-import VueRouter from 'vue-router';
 import App from './App.vue';
-import { routes } from './routes';
-
-Vue.use(VueRouter); //registering routing
-
-const router = new VueRouter({
-  routes,
-  mode: 'history', //remove # from url only if the server is configured to give you index.html
-  scrollBehavior(to, from, savePosition) {
-    // if(savedPosition) {
-    //   return savedPosition;
-    // }
-    if(to.hash) {
-      return { selector: to.hash }
-    }
-    return { x: 0, y: 700 }
-  }
-});
-//executes on each router actions
-router.beforeEach((to, from, next) => {
-  next(); //to continue with the next path pass path if needs to redirect
-})
 
 new Vue({
   el: '#app',
-  router,
   render: h => h(App)
 })
 
